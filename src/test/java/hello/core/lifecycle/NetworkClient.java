89c253d65a72b17b08251c79f488b011843289ca
김규동 <kyudong_kim@tmax.co.kr>
2023-01-09 13:12:08 +0900

    add: ~ 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,10 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -28,15 +29,17 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     public void setUrl(String url) {
         this.url = url;
     }
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
